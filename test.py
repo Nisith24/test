b1b8a945417e8d4c7db6b076bc977e957493263c
Nisith24 <nisithpraveen@gmail.com>
2021-10-05 14:10:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,11 +13,11 @@ chrome_options.add_argument("--no-sandbox")
 
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = 
 
-driver.get("https://medium.com")
 
-print(driver.page_source)
+
+
 
 print("Finished!")
 
